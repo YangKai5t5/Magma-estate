04cebf9716a3f2e2c6855fdb24047deae301e2eb
YangKai5t5 <test7t7@gmail.com>
2025-05-20 20:26:21 +0530

    Update db.js

--- api/utils/db.js
+++ api/utils/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = () => {
   mongoose
     .connect(
-  "mongodb+srv://aftab7t7:fate100blood@cluster0.kp2a8.mongodb.net/MCAminiproject?retryWrites=true&w=majority&appName=Cluster0"
+  "mongodb+srv://aftab7t7:fatblood@cluster0.kp2a8.mongodb.net/MCAminiproject?retryWrites=true&w=majority&appName=Cluster0"
     )
     .then((c) => console.log(`DB Connected to ${c.connection.host}`))
     .catch((err) => {
